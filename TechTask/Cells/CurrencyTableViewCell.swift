ed9b88b2946b1d5f304dd3b5866a5a6e4f4f46a3
Vitalmih <vital.mih@icloud.com>
2021-01-05 15:24:35 +0200

    add currency view controller

---
+++ TechTask/Cells/CurrencyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyTableViewCell.swift
+//  TechTask
+//
+//  Created by Виталий on 05.01.2021.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
