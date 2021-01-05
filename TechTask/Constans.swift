7f2856538eae4a4b97b53d30b28c53cddd644aee
Vitalmih <vital.mih@icloud.com>
2021-01-05 15:31:23 +0200

    add constant file

---
+++ TechTask/Constans.swift
@@ -0,0 +1,12 @@
+//
+//  Constans.swift
+//  TechTask
+//
+//  Created by Виталий on 05.01.2021.
+//
+
+import Foundation
+
+struct K {
+    static let jsonLink = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+}
---
+++ TechTask/Networking/Currency.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TechTask
+//
+//  Created by Виталий on 05.01.2021.
+//
+
+import Foundation
