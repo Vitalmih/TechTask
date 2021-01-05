df9adc3ac680fce1c55f06469272711779df0298
Vitalmih <vital.mih@icloud.com>
2021-01-05 15:08:03 +0200

    Initial Commit

---
+++ TechTask/Controllers/ConverterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConverterViewController.swift
+//  TechTask
+//
+//  Created by Виталий on 05.01.2021.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+   
+}
---
+++ TechTask/Controllers/CurrenciesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CurrenciesViewController.swift
+//  TechTask
+//
+//  Created by Виталий on 05.01.2021.
+//
+
+import UIKit
+
+class CurrenciesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
